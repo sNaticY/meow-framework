f1a141806ca0fff78bf8e73dc962601dcba0684c
sNatic <snatic@outlook.com>
2017-07-11 02:01:27 +0800

    Upload some common components

---
+++ Assets/Meow/Components/DontDestroy.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+
+public class DontDestroy : MonoBehaviour
+{
+    void Start()
+    {
+        DontDestroyOnLoad(this);
+    }
+}
+
+
+
---
+++ Assets/Meow/Components/MonoSingleton.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+
+public class MonoSingleton<T> : MonoBehaviour where T : MonoSingleton<T>
+{
+    private static T _instance;
+
+    public static T Instance
+    {
+        get
+        {
+            if (_instance == null)
+            {
+                _instance = (T) FindObjectOfType(typeof(T));
+                if (_instance == null)
+                {
+                    GameObject obj = new GameObject(typeof(T).Name);
+                    _instance = obj.AddComponent<T>();
+                    GameObject parent = GameObject.Find("MonoSingleton");
+                    if (parent == null)
+                    {
+                        parent = new GameObject("MonoSingleton");
+                    }
+                    _instance.transform.parent = parent.transform;
+                    DontDestroyOnLoad(parent);
+                }
+            }
+            return _instance;
+        }
+    }
+}
