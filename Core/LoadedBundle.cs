f7b50bf98c3d93d8be939ca37a3f01b6e88ef1b9
sNatic <snatic@outlook.com>
2017-07-14 01:19:49 +0800

    Squashed 'Assets/AssetLoader/' content from commit 06d0dd7
    git-subtree-dir: Assets/AssetLoader
    git-subtree-split: 06d0dd70f714b73d05df6137ba19e03380e45558

---
+++ Core/LoadedBundle.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+
+namespace Meow.AssetLoader.Core
+{
+    public class LoadedBundle
+    {
+        public string AssetBundlePath;
+        public AssetBundle AssetBundle;
+        public int ReferecedCount;
+
+        public LoadedBundle(string assetBundlePath, AssetBundle assetBundle)
+        {
+            AssetBundlePath = assetBundlePath;
+            AssetBundle = assetBundle;
+            ReferecedCount = 1;
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,4 @@
+# assetLoader-core
+AssetLoader is a Unity plugin which helps developers load and manage assets from assetbundles easily
+
+Not finished yet
