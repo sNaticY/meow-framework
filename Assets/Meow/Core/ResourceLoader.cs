6a741ffe7bd7d3b1e8a447d4e882c690b45ca7f1
sNatic <snatic@outlook.com>
2017-07-14 00:53:59 +0800

    Add Resource loader to load resource without assetbundle name

---
+++ Assets/Meow/Core/ResourceLoader.cs
@@ -0,0 +1,24 @@
+﻿using Meow.AssetLoader;
+using Meow.AssetLoader.Core;
+using Meow.AssetUpdater;
+
+namespace Meow.Framework
+{
+    public class ResourceLoader
+    {
+        private MainLoader _loader;
+        private MainUpdater _updater;
+        
+        public ResourceLoader(MainLoader loader, MainUpdater updater)
+        {
+            _loader = loader;
+            _updater = updater;
+        }
+
+        public LoadLevelOperation LoadLevelAsync(string levelPath, bool isAdditive = false)
+        {
+            var bundleName = _updater.GetAssetbundleNameByAssetPath(levelPath.ToLower());
+            return _loader.LoadLevel(bundleName, levelPath, isAdditive);
+        }
+    }
+}
